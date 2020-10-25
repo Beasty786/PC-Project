3cf7040cedd3c34351fdda3656ae416bf64f31fd
Beasty786 <shameelnkosi@gmail.com>
2020-10-25 03:47:54 +0200

    sequential Bitonic sort initialization done

---
+++ src/Bitonic Sort/Bitonic_Parallel.c
@@ -0,0 +1,134 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <mpi.h>
+
+bool isBitonic(int *arr , int size , int index);
+void swap(int* arr , int i , int j);
+void printArr(int *arr , int size);
+int changes(int * arr , int size);
+void arrUpdate(int *arr , int size , int index);
+
+int main(int argc, char const *argv[])
+{
+    int arr[9] = {1 , 2 ,  4 ,5,9,8,7,6};
+    int size = sizeof(arr)/ sizeof(int);
+
+   
+    return 0;
+}
+
+int changes(int *arr , int size){
+    int count = 0;
+    int index = -1;
+    bool isIncreasing = false;
+    if(arr[0] < arr[1]){
+        isIncreasing = true;
+    }
+
+        int i;
+    for (i = 0; i < size-1; i++)
+    {
+      if (isIncreasing)
+      {
+        if(arr[i+1] < arr[i]){
+            isIncreasing = false;
+            index = i+1;
+            count++;
+        }
+      }
+      else
+      {
+        if(arr[i+1] > arr[i]){
+            isIncreasing = true;
+            index = i+1;
+            count ++;
+        }
+      }     
+    }
+
+    return count < 3 ? index : -1;
+}
+
+void arrUpdate(int* arr , int size , int index){
+    int newArr[size];
+    for (size_t i = 0; i < size; i++)
+    {
+        newArr[i] = arr[index%size];
+        index ++;
+    }
+
+    for (size_t i = 0; i < size; i++)
+    {
+        arr[i] = newArr[i];
+    }
+    
+}
+
+bool isBitonic(int* arr , int size , int index){
+    bool bitonic = true;
+    
+    bool isIncreasing = false;
+    if(arr[0] < arr[1]){
+        isIncreasing = true;
+    }
+
+    int i;
+    for (i = 0; i < size-1; i++)
+    {
+      if (isIncreasing)
+      {
+          if(arr[i+1] < arr[i]){
+              isIncreasing = false;
+              i = i + 1;
+              break;
+          }
+      }
+      else
+      {
+           if(arr[i+1] > arr[i]){
+              isIncreasing = true;
+              i = i + 1;
+              break;
+            }
+      }     
+    }
+
+    for (size_t j = i; j < size-1; j++)
+    {
+        if(isIncreasing){
+            if (arr[j] > arr[j+1])
+            {
+                bitonic = false;
+                break;
+            }
+            
+        }
+        else
+        {
+            if (arr[j] < arr[j+1])
+            {
+                bitonic = false;
+                break;
+            }
+        }
+        
+    }
+    
+
+    return bitonic;
+}
+
+void swap(int *arr , int i , int j){
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+void printArr(int *arr , int size){
+    for (size_t i = 0; i < size; i++)
+    {
+        printf("%d " , arr[i]);
+    }
+    printf("\n");
+}
